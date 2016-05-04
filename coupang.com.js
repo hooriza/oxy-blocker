e903aa0e9b75ceaa1f29e2f59c6d68e55e5f239f
Heejae Kim <heejae.kim@navercorp.com>
2016-05-04 14:59:09 +0900

    support coupang.com #4

---
+++ coupang.com.js
@@ -0,0 +1,28 @@
+var css = `
+  .--oxy-blocker {
+    {{css:item}}
+  }
+
+  .--oxy-blocker::after {
+    {{css:image}}
+  }
+
+  .--oxy-blocker > .detail-link > img,
+  .--oxy-blocker > .detail-link > .thumbnail {
+    {{css:cover}}
+  }
+
+  #productList > li > * {
+    {{css:transition}}
+  }
+
+  .--oxy-blocker > * {
+    {{css:hide}}
+  }
+`;
+
+OxyBlocker.initialize(css, {
+  wrap: '.search-content',
+  list: '#productList > li:not(.--oxy-blocker-processed)',
+  title: '.title'
+}, true);
